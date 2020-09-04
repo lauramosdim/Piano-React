e2ec60b58f671319768cc92a4eb6067f6b345a22
Laura <lauramosdim7@gmail.com>
2020-09-04 08:50:09 -0500

    piano en react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import heart from "./descarga.png";
 
 function App() {
+
+  const [estaSonando, setEstaSonando] = useState("");//variable que me va a decir qué nota está sonando
+
+  const notas = [
+    {
+      nombre: "Do",
+      link: require("./notas/do.wav"),
+      tieneSostenido: true,
+    },
+    {
+      nombre: "Re",
+      link: require("./notas/re.wav"),
+      tieneSostenido: true,
+    },
+    {
+      nombre: "Mi",
+      link: require("./notas/mi.wav"),
+    },
+    {
+      nombre: "Fa",
+      link: require("./notas/fa.wav"),
+      tieneSostenido: true,
+    },
+    {
+      nombre: "Sol",
+      link: require("./notas/sol.wav"),
+      tieneSostenido: true,
+    },
+    {
+      nombre: "La",
+      link: require("./notas/la.wav"),
+      tieneSostenido: true,
+    },
+    {
+      nombre: "Si",
+      link: require("./notas/si.wav"),
+    },
+
+  ]
+
+  const handleClick = (nota) => {
+
+    //console.log(nota);
+    setEstaSonando(nota.nombre);//cuando haga clic vamos a reemplazar la variable por el nombre de la nota
+    const sonido = new Audio(nota.link);
+    sonido.play();
+
+    setTimeout(() => { setEstaSonando("") }, 300)
+
+    console.log(estaSonando);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="titulo">
+        <h1>Piano con React</h1>
+        <h2>Hecho con amor <img src={heart} alt="imagen corazón" className="corazon"></img> por Malala</h2>
+        {estaSonando && <h3>Está sonando la nota {estaSonando}</h3>}
+      </div>
+
+      <div className="contenedor">
+        {
+          notas.map(nota => {
+            return (
+              <div
+                className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`}
+                onClick={() => handleClick(nota)}>
+                {nota.tieneSostenido && <div className="negra"></div>}
+              </div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
